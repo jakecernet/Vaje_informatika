e78fae0b620ab5ac1bd8ffac6e4d52312de15540
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-05-28 16:49:35 +0200

    onclick function

--- Vaja6/Vrhnika/script.js
+++ Vaja6/Vrhnika/script.js
@@ -1,9 +1,31 @@
+element1 = document.getElementById("1");
+element2 = document.getElementById("2");
+element3 = document.getElementById("3");
+element4 = document.getElementById("4");
+element5 = document.getElementById("5");
+element6 = document.getElementById("6");
+
+element1.style.width = "30%";
+element1.style.height = "30%";
+element2.style.width = "30%";
+element2.style.height = "30%";
+element3.style.width = "30%";
+element3.style.height = "30%";
+element4.style.width = "30%";
+element4.style.height = "30%";
+element5.style.width = "30%";
+element5.style.height = "30%";
+element6.style.width = "30%";
+element6.style.height = "30%";
+
 function zoom(slika) {
     var element = document.getElementById(slika);
-    if (element.style.width == "100%") {
-        element.style.width = "150%";
+    if (element.style.width == "30%") {
+        element.style.width = "80%";
+        element.style.height = "80%";
     }
-    if (element.style.width == "150%") {
-        element.style.width = "100%";
+    else {
+        element.style.width = "30%";
+        element.style.height = "30%";
     }
 }
